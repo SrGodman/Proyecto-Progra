d1b8e37ce3417b41f5a7909d6c06a2dee1684120
SrGodman <erickandrehernandezortiz@gmail.com>
2026-03-22 18:32:16 -0600

    Se arreglaron bugs y ahora el formulario ya se despliega al correr el programa

--- src/main/java/com/mycompany/proyecto_progra/Proyecto_Progra.java
+++ src/main/java/com/mycompany/proyecto_progra/Proyecto_Progra.java
@@ -11,7 +11,10 @@ package com.mycompany.proyecto_progra;
 public class Proyecto_Progra {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Lamama de la mama");
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new login().setVisible(true);
+            }
+        });
     }
 }
